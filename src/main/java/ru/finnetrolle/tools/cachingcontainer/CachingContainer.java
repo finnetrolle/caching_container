011d64ad9af174a993101d3118314b1cfc4507c9
finnetrolle <maxsyachin@gmail.com>
2015-11-24 23:12:49 +0300

    + some javaDoc

--- src/main/java/ru/finnetrolle/tools/cachingcontainer/CachingContainer.java
+++ src/main/java/ru/finnetrolle/tools/cachingcontainer/CachingContainer.java
@@ -5,17 +5,40 @@ import java.util.function.Supplier;
 /**
  * Created by finnetrolle on 24.11.2015.
  */
+
+/**
+ * Container to help you cache your objects
+ * @param <T> - any type derives Object
+ */
 public class CachingContainer<T> {
 
+    /**
+     * Value for one second
+     */
     public static final long SECONDS = 1000L;
+    /**
+     * Value for one minute
+     */
     public static final long MINUTES = 60 * SECONDS;
+    /**
+     * Value for one hour
+     */
     public static final long HOURS = 60 * MINUTES;
+    /**
+     * Value for one day
+     */
     public static final long DAYS = 24 * HOURS;
 
     private final long livingTime;
     private volatile long updatedTimstamp = 0L;
     private T value;
 
+    /**
+     * Factory method to create caching object
+     * @param livingTime time in millis you want value to live
+     * @param <T> type of value
+     * @return instance of caching container
+     */
     public static <T> CachingContainer<T> build(long livingTime) {
         return new CachingContainer<>(livingTime);
     }
@@ -38,18 +61,33 @@ public class CachingContainer<T> {
         return value;
     }
 
+    /**
+     * Method allows you to provide supplier method with checked exceptions
+     * @param supplier supplier method
+     * @return value
+     * @throws Exception
+     */
     public T getChecked(CheckedSupplier<T> supplier) throws Exception {
         return (isInitialized() && isUpToDate())
                 ? value
                 : update(supplier.get());
     }
 
+    /**
+     * Method checks time of last access and load value from supplier if TTL expires or from cached value
+     * @param supplier supplier method
+     * @return value
+     */
     public T get(Supplier<T> supplier) {
         return (isInitialized() && isUpToDate())
                 ? value
                 : update(supplier.get());
     }
 
+    /**
+     * Override this method in your derived class if you want to check age of cached value
+     * @return time in millis
+     */
     protected long getAge() {
         return (isInitialized()) ? System.currentTimeMillis() - livingTime : 0L;
     }
